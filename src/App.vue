612ecb103e40c7bc76d2e5ac4b0663fc99657bbb
DarioEsposito89 <darioesposito.work@gmail.com>
2023-08-21 19:51:23 +0200

    Aggiunto traccia e finito esercizio

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,8 @@
+<!-- Create un nuovo progetto utilizzando Vite: aiutatevi con le slide per ripercorrere i vari passaggi dell’installazione come visti a lezione.
+Create e utilizzate un componente AppTitle, il quale contiene un titolo che recita “La mia prima app con Vite!”
+Bonus:
+Creare un secondo componente AppHero, che visualizza un jumbotron. -->
+
 <script>
 import AppTitle from './components/AppTitle.vue';
 import AppHero from './components/AppHero.vue';
